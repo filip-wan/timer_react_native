9f8f9ed86faaaddb48d03ae2d00ad4c472b8e2d9
Filip Wanat <filip.k.wanat@gmail.com>
2020-10-31 17:40:19 +0100

    ✨ Add seconds timer buttons

--- src/components/Pages/Timer.js
+++ src/components/Pages/Timer.js
@@ -18,22 +18,21 @@ const Timer = ({}) => {
   const [startTime, setStartTime] = useState(null);
 
   const getTime = () => {
-    if(timeElapsed < 0) {
+    if (timeElapsed < 0) {
       Vibration.vibrate(100);
-      return "Time Is Up!"
+      return 'Time Is Up!';
     }
     const seconds = Math.floor((timeElapsed || time) / 1000);
     const minutes = Math.floor(seconds / 60);
     const hours = Math.floor(minutes / 60);
-    return `${hours % 12} : ${minutes % 60} : ${seconds%60}`
-  }
+    return `${hours % 12} : ${minutes % 60} : ${seconds % 60}`;
+  };
 
-
-  const timerCallback = (timestamp) => () =>{
-    if(isGoing) return;
-    const timeElapsedToSet = time - (Date.now() - timestamp)
-    setTimeElapsed( timeElapsedToSet );
-  }
+  const timerCallback = (timestamp) => () => {
+    if (isGoing) return;
+    const timeElapsedToSet = time - (Date.now() - timestamp);
+    setTimeElapsed(timeElapsedToSet);
+  };
 
   const onPressStart = () => {
     setIsGoing(!isGoing);
@@ -45,40 +44,70 @@ const Timer = ({}) => {
       clearInterval(timerInterval);
       const timeToSet = time - (Date.now() - startTime);
       setTime(timeToSet < 0 ? 0 : timeToSet);
-      setTimeElapsed(0)
-      setStartTime(null)
+      setTimeElapsed(0);
+      setStartTime(null);
     }
   };
 
   const onPressTime = (ammount) => () => {
-    const timeToSet = time + ammount
+    const timeToSet = time + ammount;
     setTime(timeToSet > 0 ? timeToSet : 0);
   };
 
   return (
     <View>
-    <Text style={styles.time}>{getTime()}</Text>
-    <View style={styles.buttons}>
-      <TouchableOpacity onPress={onPressStart} style={styles.button}>
-        <Text style={styles.text}>{isGoing ? 'Stop' : 'Start'}</Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.buttons}>
-      <TouchableOpacity onPress={onPressTime(1000 * 6)} style={styles.button}>
-        <Text style={styles.text}>{'+1min'}</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onPressTime(- 1000 * 60)} style={styles.button}>
-        <Text style={styles.text}>{'-1min'}</Text>
-      </TouchableOpacity>
-    </View>
-    <View style={styles.buttons}>
-      <TouchableOpacity onPress={onPressTime(1000 * 600)} style={styles.button}>
-        <Text style={styles.text}>{'+10min'}</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={onPressTime(- 1000 * 600)} style={styles.button}>
-        <Text style={styles.text}>{'-10min'}</Text>
-      </TouchableOpacity>
-    </View>
+      <Text style={styles.time}>{getTime()}</Text>
+      <View style={styles.buttons}>
+        <TouchableOpacity onPress={onPressStart} style={styles.button}>
+          <Text style={styles.text}>{isGoing ? 'Stop' : 'Start'}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity onPress={onPressTime(1000 * 1)} style={styles.button}>
+          <Text style={styles.text}>{'+1s'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onPressTime(-1000 * 1)}
+          style={styles.button}>
+          <Text style={styles.text}>{'-1s'}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          onPress={onPressTime(1000 * 10)}
+          style={styles.button}>
+          <Text style={styles.text}>{'+10s'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onPressTime(-1000 * 10)}
+          style={styles.button}>
+          <Text style={styles.text}>{'-10s'}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          onPress={onPressTime(1000 * 60)}
+          style={styles.button}>
+          <Text style={styles.text}>{'+1min'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onPressTime(-1000 * 60)}
+          style={styles.button}>
+          <Text style={styles.text}>{'-1min'}</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity
+          onPress={onPressTime(1000 * 600)}
+          style={styles.button}>
+          <Text style={styles.text}>{'+10min'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={onPressTime(-1000 * 600)}
+          style={styles.button}>
+          <Text style={styles.text}>{'-10min'}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
